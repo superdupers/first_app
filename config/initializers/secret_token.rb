30a3949b9a4a6d8b7b1c6785681cb9e50c72fdc9
cesar <cesarhernandezcasas@gmail.com>
2013-11-25 08:58:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TecWeb::Application.config.secret_token = '8d4ec58e97ff609d080b39e78d9228476ca73f9545a734d1fbd00463d0c703b096eeb81d636a39afd512fa43d3a12f9b102373f1ae53f90ec0a09ff617e84a76'
